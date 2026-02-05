8bff3590d55409c8f58a92793a248a744bc1178e
udaykiran000 <58301257+udaykiran000@users.noreply.github.com>
2026-02-06 00:53:07 +0530

    Enhance sendEmail function with environment checks
    Added console logs to verify environment variable presence and length for email configuration.

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,13 +1,21 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async (email, subject, text) => {
+  //
+  console.log("--- Check Environment Variables ---");
+  console.log("EMAIL_USER exists:", process.env.EMAIL_USER ? "YES ✅" : "NO ❌");
+  console.log("EMAIL_PASS length:", process.env.EMAIL_PASS ? process.env.EMAIL_PASS.length : 0);
+
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail', // Render 
+      host: "smtp.gmail.com",
+      port: 465, // 
+      secure: true, 
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_PASS?.replace(/\s/g, ""), // 
       },
+      connectionTimeout: 10000, // 10 
     });
 
     await transporter.sendMail({
