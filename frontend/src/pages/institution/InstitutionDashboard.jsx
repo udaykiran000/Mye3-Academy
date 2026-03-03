3cd00efcb49768a69a0b812848dbfc92aafe37d4
RAKESH-KUMAR-N3 <nrakeshkumar36@gmail.com>
2026-03-03 12:20:52 +0530

    changes on 2nd march

--- frontend/src/pages/institution/InstitutionDashboard.jsx
+++ frontend/src/pages/institution/InstitutionDashboard.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import InstitutionSidebar from "../../components/institution/InstitutionSidebar";
 
 const InstitutionDashboard = () => {
+  const mainRef = useRef(null);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (mainRef.current) {
+      mainRef.current.scrollTo(0, 0);
+    }
+  }, [pathname]);
+
   return (
     <div className="flex h-screen bg-gray-50 overflow-hidden">
       <InstitutionSidebar />
-      <main className="flex-1 overflow-y-auto w-full focus:outline-none scroll-smooth">
+      <main ref={mainRef} className="flex-1 overflow-y-auto w-full focus:outline-none scroll-smooth">
         <div className="mx-auto">
           <Outlet />
         </div>
